9721fdd9caf2b200dd43ebf4edf3700193acc33a
ashishgaikwad29 <ashishgaikwad479@gmail.com>
2026-06-15 23:40:40 +0530

    Added Python practice programs and assignments

---
+++ Python Fundamentals/program2_6.py
@@ -0,0 +1,23 @@
+#   Write a program that accepts two numbers from user and prints their:
+#   • Addition
+#   • Subtraction
+#   • Multiplication
+#   • Division
+
+print("Enter Your first Number : ")
+No1 = float(input())
+
+print("Enter Your Second Number : ")
+No2 = float(input())
+
+Ans1 = No1 + No2
+print("Your Addition is : ",Ans1)
+
+Ans2 = No1 - No2
+print("Your Substraction is : ",Ans2)
+
+Ans3 = No1 * No2
+print("Your Multiplication is :",Ans3)
+
+Ans4 = No1 / No2
+print("Your Division is : ",Ans4)
